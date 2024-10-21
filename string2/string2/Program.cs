72c00170023dae749f84d7b02cd811808a3d8e32
LeonCicak <leoncicak07@gmail.com>
2024-10-21 13:40:39 +0200

    Add files via upload

---
+++ string2/string2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi niz znakova");
+            string input = Console.ReadLine();
+            string output = input.Replace(" ", "_");
+            Console.WriteLine("Rezultat " + output);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
